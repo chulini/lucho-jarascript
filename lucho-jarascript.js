01200cd5a9b687ad0ed9202bcfa6912cdccd1e62
Julio Quiroz <chulini@gmail.com>
2015-12-30 18:57:32 -0300

    ahora reemplaza todos los textos por un golpe de suerte

--- lucho-jarascript.js
+++ lucho-jarascript.js
@@ -1,12 +1,39 @@
 var $j = jQuery.noConflict();
 
 $j( document ).ready(function() {
+    $j('h1').each(function(){
+        $j(this).html("Luis Jara");
+    });
+
+    $j('h2, h3, h4, h5, h6').each(function(){
+        $j(this).html("Un golpe de suerte");
+    });
+
+    $j('p').each(function(){
+        var text = $j(this).text();
+        $j(this).html(textToGolpeDeSuerte(text));
+    });
+
 
     $j('img').each(function(){
         $j(this).attr("src", randomLucho());
     });
-    
 });
+
+function textToGolpeDeSuerte(text){
+    var golpe = "Un golpe de suerte eh eh eh, espero despierto... igual que un espia en la oscuridad, espero volver a encontrar, un golpe de suerte, podria cambiar el tiempo perdido, que hay en el vacio que dejaste ayer al partir, un golpe de suerte eres tuu, Soy, un tipo tranquilo, tratando de no hacer mal, y ser buen amigo, yo soy uno mas, uno de tantos, en busca de ser feliz, de vez en cuando..., y con el tiempo de caminar, fui aprendiendo, ahora se que puedo llegar si me dan tiempo. Soy, un caso perdido, luchando para quebrarle la mano al destino, vivir a diario, es tan bulnerable, cuantas noches sin dormir, ardiendo de frioo, y con el tiempo de caminar, fui aprendiendo, ahora se que puedo, llegar si me doy tiempo";
+    var golpe_words = golpe.split(" ");
+
+    var text_words = text.split(" ");
+    var replaced_text = "";
+    for(var i = 0; i < text_words.length;i++)
+        replaced_text += golpe_words[i%golpe_words.length]+" ";
+
+    return replaced_text;
+
+}
+
+//Returns random image of lucho jara
 function randomLucho(){
     var luchos = [];
     luchos.push("http://i.imgur.com/eRC8onz.jpg");
