7a1b173dfe4cd200a64add62e27ca69375f3a477
Gustavo Alfredo Marín Sáez <aterbonus@gmail.com>
2016-01-06 20:45:53 -0300

    Agregadas las funciones randomLucho() y textToGolpeDeSuerte() al scope del objeto.

--- lucho-jarascript.js
+++ lucho-jarascript.js
@@ -5,6 +5,57 @@
 
         var este = this;
 
+        //Array of lucho's images
+        var luchos = [];
+        luchos.push("http://i.imgur.com/eRC8onz.jpg");
+        luchos.push("http://i.imgur.com/eISN50J.jpg");
+        luchos.push("http://i.imgur.com/nfL1ay3.jpg");
+        luchos.push("http://i.imgur.com/eYBo65E.jpg");
+        luchos.push("http://i.imgur.com/WMTJa8v.jpg");
+        luchos.push("http://i.imgur.com/llTC4uD.jpg");
+        luchos.push("http://i.imgur.com/yBBoQzj.jpg");
+        luchos.push("http://i.imgur.com/WAJbX1o.jpg");
+        luchos.push("http://i.imgur.com/KBM537X.jpg");
+        luchos.push("http://i.imgur.com/fxtpaM2.jpg");
+        luchos.push("http://i.imgur.com/91KCiE4.jpg");
+        luchos.push("http://i.imgur.com/iWys51v.jpg");
+        luchos.push("http://i.imgur.com/rT6ybOE.jpg");
+        luchos.push("http://i.imgur.com/4iST79y.jpg");
+        luchos.push("http://i.imgur.com/0Ce3339.jpg");
+        luchos.push("http://i.imgur.com/F3uq7wg.jpg");
+        luchos.push("http://i.imgur.com/obsJjoW.jpg");
+        luchos.push("http://i.imgur.com/Svs3fP9.jpg");
+        luchos.push("http://i.imgur.com/Fv75FGB.png");
+        luchos.push("http://i.imgur.com/0LmhWV2.jpg");
+        luchos.push("http://i.imgur.com/75inuSW.jpg");
+        luchos.push("http://i.imgur.com/hnDV1Cy.jpg");
+        luchos.push("http://i.imgur.com/xm56egI.jpg");
+        luchos.push("http://i.imgur.com/wcNMFHq.jpg");
+        luchos.push("http://i.imgur.com/M8Vsx8b.jpg");
+        luchos.push("http://i.imgur.com/mSWmxSz.jpg");
+        luchos.push("http://i.imgur.com/Aei4AAF.jpg");
+        luchos.push("http://i.imgur.com/jkB4Zn5.jpg");
+
+        //Un golpe de suerteeeee, eeeeeeEEEEEEeeeeeHHHH
+        var golpe = "Un golpe de suerte, espero despierto... igual que un espia en la oscuridad, espero volver a encontrar, un golpe de suerte, podria cambiar el tiempo perdido, que hay en el vacio que dejaste ayer al partir, un golpe de suerte eres tu. Soy, un tipo tranquilo, tratando de no hacer mal, y ser buen amigo, yo soy uno mas, uno de tantos, en busca de ser feliz, de vez en cuando... y con el tiempo de caminar, fui aprendiendo, ahora se que puedo llegar si me dan tiempo. Soy, un caso perdido, luchando para quebrarle la mano al destino, vivir a diario, es tan bulnerable, cuantas noches sin dormir, ardiendo de frioo, y con el tiempo de caminar, fui aprendiendo, ahora se que puedo, llegar si me doy tiempo";
+        var golpe_words = golpe.split(" ");
+
+        //Returns random image of lucho jara
+        function randomLucho(){
+            return luchos[Math.floor(Math.random()*luchos.length)];
+        }
+
+        //Converts regular text to un golpe de suerte lyrics
+        function textToGolpeDeSuerte(text){
+            var text_words = text.split(" ");
+            var replaced_text = "";
+            for(var i = 0; i < text_words.length;i++)
+                replaced_text += golpe_words[i%golpe_words.length]+" ";
+
+            return replaced_text;
+
+        }
+
         este.find('h1').each(function () {
             $(this).html("Luis Jara");
         });
@@ -52,51 +103,3 @@
 /*jQuery(document).ready(function($){
     $('body').luchoJaraScript();
 });*/
-
-//Converts regular text to un golpe de suerte lyrics
-function textToGolpeDeSuerte(text){
-    var golpe = "Un golpe de suerte, espero despierto... igual que un espia en la oscuridad, espero volver a encontrar, un golpe de suerte, podria cambiar el tiempo perdido, que hay en el vacio que dejaste ayer al partir, un golpe de suerte eres tu. Soy, un tipo tranquilo, tratando de no hacer mal, y ser buen amigo, yo soy uno mas, uno de tantos, en busca de ser feliz, de vez en cuando... y con el tiempo de caminar, fui aprendiendo, ahora se que puedo llegar si me dan tiempo. Soy, un caso perdido, luchando para quebrarle la mano al destino, vivir a diario, es tan bulnerable, cuantas noches sin dormir, ardiendo de frioo, y con el tiempo de caminar, fui aprendiendo, ahora se que puedo, llegar si me doy tiempo";
-    var golpe_words = golpe.split(" ");
-
-    var text_words = text.split(" ");
-    var replaced_text = "";
-    for(var i = 0; i < text_words.length;i++)
-        replaced_text += golpe_words[i%golpe_words.length]+" ";
-
-    return replaced_text;
-
-}
-
-//Returns random image of lucho jara
-function randomLucho(){
-    var luchos = [];
-    luchos.push("http://i.imgur.com/eRC8onz.jpg");
-    luchos.push("http://i.imgur.com/eISN50J.jpg");
-    luchos.push("http://i.imgur.com/nfL1ay3.jpg");
-    luchos.push("http://i.imgur.com/eYBo65E.jpg");
-    luchos.push("http://i.imgur.com/WMTJa8v.jpg");
-    luchos.push("http://i.imgur.com/llTC4uD.jpg");
-    luchos.push("http://i.imgur.com/yBBoQzj.jpg");
-    luchos.push("http://i.imgur.com/WAJbX1o.jpg");
-    luchos.push("http://i.imgur.com/KBM537X.jpg");
-    luchos.push("http://i.imgur.com/fxtpaM2.jpg");
-    luchos.push("http://i.imgur.com/91KCiE4.jpg");
-    luchos.push("http://i.imgur.com/iWys51v.jpg");
-    luchos.push("http://i.imgur.com/rT6ybOE.jpg");
-    luchos.push("http://i.imgur.com/4iST79y.jpg");
-    luchos.push("http://i.imgur.com/0Ce3339.jpg");
-    luchos.push("http://i.imgur.com/F3uq7wg.jpg");
-    luchos.push("http://i.imgur.com/obsJjoW.jpg");
-    luchos.push("http://i.imgur.com/Svs3fP9.jpg");
-    luchos.push("http://i.imgur.com/Fv75FGB.png");
-    luchos.push("http://i.imgur.com/0LmhWV2.jpg");
-    luchos.push("http://i.imgur.com/75inuSW.jpg");
-    luchos.push("http://i.imgur.com/hnDV1Cy.jpg");
-    luchos.push("http://i.imgur.com/xm56egI.jpg");
-    luchos.push("http://i.imgur.com/wcNMFHq.jpg");
-    luchos.push("http://i.imgur.com/M8Vsx8b.jpg");
-    luchos.push("http://i.imgur.com/mSWmxSz.jpg");
-    luchos.push("http://i.imgur.com/Aei4AAF.jpg");
-    luchos.push("http://i.imgur.com/jkB4Zn5.jpg");
-    return luchos[Math.floor(Math.random()*luchos.length)];
-}
